9e0592020b8787152497bdf066f085f3759c8834
tommylay1902 <tommylay.c@gmail.com>
2025-10-20 12:25:03 -0700

    started impl of sound bar for audio player

--- src/modules/study/ui/components/audio/volume-control.tsx
+++ src/modules/study/ui/components/audio/volume-control.tsx
@@ -1,40 +1,41 @@
 "use client";
 
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { VolumeOff, Volume2 } from "lucide-react";
 interface VolumeControlProps {
-    isMuted: boolean;
-    handleMuteChange: (isMuted: boolean) => void;
+  isMuted: boolean;
+  handleMuteChange: (isMuted: boolean) => void;
 }
 const VolumeControl: React.FC<VolumeControlProps> = ({
-    isMuted,
-    handleMuteChange,
+  isMuted,
+  handleMuteChange,
 }) => {
-    return (
-        <>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <div
-                        onClick={() => handleMuteChange(!isMuted)}
-                        className="cursor-pointer"
-                    >
-                        {isMuted ? (
-                            <VolumeOff size={48} />
-                        ) : (
-                            <Volume2 size={48} />
-                        )}
-                    </div>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>Will play rain noises when working pomo is activated</p>
-                </TooltipContent>
-            </Tooltip>
-        </>
-    );
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="flex flex-row justify-items-center items-center gap-x-3">
+            <div
+              onClick={() => handleMuteChange(!isMuted)}
+              className="cursor-pointer"
+            >
+              {isMuted ? <VolumeOff size={48} /> : <Volume2 size={48} />}
+            </div>
+            <div>
+              <input type="range" step="1" min="0" max="100" />
+            </div>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Will play rain noises when working pomo is activated</p>
+        </TooltipContent>
+      </Tooltip>
+    </>
+  );
 };
 
 export default VolumeControl;
