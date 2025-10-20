9ec5711f44bc8e3cf6e76d4c45c719b969f4c052
tommylay1902 <tommylay.c@gmail.com>
2025-10-20 14:11:50 -0700

    impl basic input volume control for audio element

--- src/modules/study/ui/components/audio/index.tsx
+++ src/modules/study/ui/components/audio/index.tsx
@@ -1,24 +1,30 @@
-import React, { forwardRef } from "react";
+import React, { ChangeEvent, forwardRef } from "react";
 import AudioPlayer from "./audio-player";
 import VolumeControl from "./volume-control";
 
 interface AudioProps {
-    isMuted: boolean;
-    handleMuteChange: (isMuted: boolean) => void;
+  isMuted: boolean;
+  handleMuteChange: (isMuted: boolean) => void;
 }
 
 const Audio = forwardRef<HTMLAudioElement, AudioProps>(
-    ({ isMuted, handleMuteChange }, ref) => {
-        return (
-            <div className={`flex items-center gap-4 `}>
-                <AudioPlayer ref={ref} isMuted={isMuted} />
-                <VolumeControl
-                    isMuted={isMuted}
-                    handleMuteChange={handleMuteChange}
-                />
-            </div>
-        );
-    },
+  ({ isMuted, handleMuteChange }, ref) => {
+    const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+      if (!ref || !("current" in ref) || !ref.current || !event.target) return;
+      ref.current.volume = +event.target.value;
+      console.table(ref.current.volume);
+    };
+    return (
+      <div className={`flex items-center gap-4 `}>
+        <AudioPlayer ref={ref} isMuted={isMuted} />
+        <VolumeControl
+          isMuted={isMuted}
+          handleMuteChange={handleMuteChange}
+          handleVolumeChange={handleVolumeChange}
+        />
+      </div>
+    );
+  },
 );
 
 Audio.displayName = "audio";
--- src/modules/study/ui/components/audio/volume-control.tsx
+++ src/modules/study/ui/components/audio/volume-control.tsx
@@ -1,32 +1,47 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { VolumeOff, Volume2 } from "lucide-react";
+import { ChangeEvent, useState } from "react";
 interface VolumeControlProps {
   isMuted: boolean;
   handleMuteChange: (isMuted: boolean) => void;
+  handleVolumeChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 const VolumeControl: React.FC<VolumeControlProps> = ({
   isMuted,
   handleMuteChange,
+  handleVolumeChange,
 }) => {
+  const [volume, setVolume] = useState("1");
   return (
     <>
       <Tooltip>
         <TooltipTrigger asChild>
-          <div className="flex flex-row justify-items-center items-center gap-x-3">
+          <div className="flex flex-row justify-items-center items-center gap-x-3 group my-3">
             <div
               onClick={() => handleMuteChange(!isMuted)}
               className="cursor-pointer"
             >
               {isMuted ? <VolumeOff size={48} /> : <Volume2 size={48} />}
             </div>
-            <div>
-              <input type="range" step="1" min="0" max="100" />
+            <div className="invisible group-hover:visible my-3">
+              <Input
+                type="range"
+                step="0.05"
+                min="0"
+                max="1"
+                value={volume}
+                onChange={(e) => {
+                  setVolume(e.target.value);
+                  handleVolumeChange(e);
+                }}
+              />
             </div>
           </div>
         </TooltipTrigger>
